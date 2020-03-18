032ea9282a753d3aac82c0af564890a1ca02cf24
tedelm <Ted.Elmenheim@itgarden.se>
2020-03-18 14:35:13 +0100

    First push

---
+++ Arduino/KillerDroneFromOuterSpace/KillerDroneFromOuterSpace.ino
@@ -0,0 +1,142 @@
+//https://www.youtube.com/watch?time_continue=739&v=MvhqtOEihDg&feature=emb_title
+//https://www.mediafire.com/folder/dx6kfaasyo24l/BLHeliSuite
+//-> make interface -> Arduino BLHeli Bootloader
+
+
+//Battery - > GND
+//Trigger > D2
+// Servo Signal (PULSE) > D9
+// Servo Signal (PULSE) > D10
+
+// Don't change the below setting 
+
+#include <Servo.h>
+Servo motor1;               //ESC1
+Servo motor2;               //ESC2
+Servo Feeder;               //Servo feeder
+float motorMIN = 950;       //motor min, motors disabled
+float motorMID = 1450;      //motor MID
+float motorMAX = 1960;      //motor max
+float MotorCMD = motorMIN;  //Command sent to esc
+int motor1PIN = 9;          //Arduino PIN D9
+int motor2PIN = 10;         //Arduino PIN D10
+int TogglePIN = A7;          //A High/Low toggle pin on FC, RevoltOSD is TX4 3,3v. (1023/5)*3.3=675, so to be safe we set limit to 640
+int TogglePINActive = 640;  //(1023/5)*3.3=675, so to be safe we set limit to 640
+int FeederPin = 8;          //Servo
+int TogglePINState = 0;     // variable to store the read value
+int angle = 10;             //Servo angle
+int LastAction = 0;         //when button pressed
+//
+
+void setup() {
+    Feeder.attach(FeederPin);           //Attach PIN
+    motor1.attach(motor1PIN);           //Attach PIN
+    motor2.attach(motor2PIN);           //Attach PIN
+    //pinMode(TogglePIN, INPUT);   //Set toggle pin to read
+    Serial.begin(57600);                //Serial Monitor
+    motor1.write(motorMIN);
+    motor2.write(motorMIN);
+    delay(5000);
+}
+
+void FeedDart(){
+ // from 0 to 180 degrees
+  for(angle = 10; angle < 180; angle++){                                  
+    Feeder.write(angle);               
+    delay(5);                   
+  } 
+  // from 180 to 0 degrees
+  for(angle = 180; angle > 10; angle--){                                
+    Feeder.write(angle);           
+    delay(5);       
+  } 
+delay(250);
+}
+
+void EnableMotors(){
+
+    if(LastAction == 0){
+        //Ramp up motors MAX RPM
+        for(MotorCMD = motorMIN; MotorCMD < motorMAX; MotorCMD = MotorCMD + 25){                                  
+            Serial.println(MotorCMD);
+            motor1.write(MotorCMD);
+            motor2.write(MotorCMD);              
+            delay(10);                   
+        }
+
+    }else{
+        MotorCMD = motorMAX;
+        Serial.println(MotorCMD);
+        motor1.write(MotorCMD);
+        motor2.write(MotorCMD);
+
+    }
+    
+        MotorCMD = motorMAX;
+        Serial.println(MotorCMD);
+        motor1.write(MotorCMD);
+        motor2.write(MotorCMD);
+    LastAction = 1;
+    
+}
+
+void DeactiveateMotors(){
+
+
+        //Ramp down motors min RPM
+        for(MotorCMD = motorMAX; MotorCMD > motorMIN; MotorCMD = MotorCMD - 10){                                  
+            Serial.println(MotorCMD);
+            motor1.write(MotorCMD);
+            motor2.write(MotorCMD);
+            delay(10);                   
+        }
+
+    MotorCMD = motorMIN;        //Set motors MIN RPM
+    Serial.println(MotorCMD);
+    motor1.write(MotorCMD);
+    motor2.write(MotorCMD);
+
+    LastAction = 0;
+}
+
+void ToggleState(){
+    
+    TogglePINState = analogRead(TogglePIN);    //Read TogglePIN
+    Serial.println(TogglePINState);
+    
+    if (TogglePINState > TogglePINActive){
+        
+        delay(50);
+        TogglePINState = analogRead(TogglePIN);    //Read TogglePIN
+        Serial.println(TogglePINState);
+        //Doublecheck
+        if (TogglePINState > TogglePINActive){
+
+            //Check if motors already enabled
+            if(LastAction == 1){
+                FeedDart();     //Feed dart
+            }else{
+                EnableMotors(); //Start motors
+                delay(25);     //sleep
+                FeedDart();     //Feed dart
+            }
+
+            //Set last action to Enabled
+            LastAction = 1;
+        }
+    
+    }else{
+      
+      if(LastAction == 1){
+        DeactiveateMotors(); //Start motors
+        delay(25);          //sleep
+      }
+      
+    }
+}
+
+void loop() {
+    MotorCMD = motorMIN;
+    //Check button
+    ToggleState();
+}
--- README.md
+++ README.md
@@ -1,3 +1,81 @@
 # KillerDronesFromOuterSpace
 </br>
 Have you ever wanted to shot your friends and enemies with Nerf darts launched from a drone? 
+</br>
+
+# Make
+Needed
+- [ ]  Download KillerDroneFromOuterSpace files (Arduino and STL)
+- [ ]  Download BLHeli Suite
+- [ ]  Print STL files
+- [ ]  1 x Nerf Mag.
+- [ ]  2 x Blheli_s or Blheli_32 Esc (25-30amp)
+- [ ]  2 x Brushless motor, like 2207 (6s 1600kv or 4s 2400kv)
+- [ ]  1 x Arduino Nano
+- [ ]  1 x SG90 Servo (9g servo)
+- [ ]  1 x 5v BUCK/BEC (min 25v stepdown)
+- [ ]  1 x 4s/6s Lipo
+- [ ]  3cm Thin (~1mm) Garden wire (for pulley/servo)
+
+
+# Flash BLHELI_S ESC´s
+Download: <a href='http://www.mediafire.com/file/23jnmpa0kiqry1k/BLHeliSuite16714902.zip/file'>Windows BLHELI_S Suite</a></br>
+or browse for your version: <a href='https://www.mediafire.com/folder/dx6kfaasyo24l/BLHeliSuite'>All versions</a></br>
+</br>
+1. Connect Arduino to Computer</br>
+2. Start BLHeli Suite</br>
+3. Options -> "Expert Mode"</br>
+4. Make Interfaces</br>
+5. Select "Port" eg. COM5</br>
+6. Click Arduino BLHeli BootLoader</br>
+7. After the arduino is flashed with blheli bootloader, disconnect the Arduino from computer.</br>
+8. Select "ATMEL / SILABS interface" as described, its usually "SILABS BLHELI Bootloader (USB/COM)"</br>
+9. Solder GND and Signal from ESC to Arduino GND and D2 and solder vbat+ and vbat- to ESC</br>
+10. Connect Arduino to Computer and start BLHeli Suite, click "Connect"</br>
+11. Connect Lipo (VBAT+/VBAT-)</br>
+12. Click "Flash BLHeli", select newest FW (Default)</br>
+13. Repeat steps 1-12 for both esc´s.</br>
+</br>
+
+
+# Flash BLHELI_32 ESC´s
+Download <a href='http://www.mediafire.com/file/as2n6rkbah053zp/BLHeliSuite32_32711.zip/file'>Windows BLHELI_32 Suite</a></br>
+or browse for your version: <a href='https://www.mediafire.com/folder/dx6kfaasyo24l/BLHeliSuite'>All versions</a></br>
+1. Connect Arduino to Computer</br>
+2. Start BLHeli Suite</br>
+3. Options -> "Expert Mode"</br>
+4. Make Interfaces</br>
+5. Select "Port" eg. COM5</br>
+6. Click Arduino BLHeli BootLoader</br>
+7. After the arduino is flashed with blheli bootloader, disconnect the Arduino from computer.</br>
+8. Select "ATMEL / SILABS interface" as described, its usually "SILABS BLHELI Bootloader (USB/COM)"</br>
+9. Solder GND and Signal from ESC to Arduino GND and D2 and solder vbat+ and vbat- to ESC</br>
+10. Connect Arduino to Computer and start BLHeli Suite, click "Connect"</br>
+11. Connect Lipo (VBAT+/VBAT-)</br>
+12. Click "Flash BLHeli", select newest FW (Default)</br>
+13. Repeat steps 1-12 for both esc´s.</br>
+</br>
+
+# Flash Arduino
+1. Download and istall Arduino IDE <a href='https://www.arduino.cc/en/main/software'>Link</a></br>
+2. Open Arduino IDE and open the sketch "KillerDroneFromOuterSpace.ino" </br>
+3. Select correct Arduino under </br>
+    Tools->Board = Arduino nano </br>
+    Tools->Processor = ATMega328p (Old bootloader)</br>
+    Tools->Port = COMx</br>
+    </br>
+
+# Connect to Arduino
+1. Connection:</br>
+    ESC #1 (signal) -> Arduino pin: D9</br>
+    ESC #1 (gnd, for signal) -> Arduino pin GND  </br>
+    ESC #2 (signal) -> Arduino pin: D10</br>
+    ESC #2 (gnd, for signal) -> Arduino pin GND</br>
+    Servo (signal)  -> Arduino pin: D8</br>
+    Servo (gnd)  -> 5v BUCK/BEC GND out</br>
+    Servo (Vcc/+)  -> 5v BUCK/BEC Vcc/+ out</br>
+    RevoltOSD TX4 (Pin Toggle) -> Arduino pin: A7</br>
+    5v Buck/BEC (IN GND/VCC) -> VBAT GND/VCC</br>
+
+```code
+```
